9fee85f9990ab59f74663102fc8547d1212bec68
Marti D <marti.dolce@29signals.org>
2023-01-22 03:30:31 -0500

    Developed globals module
    exported globals
    establishes imported packages in one file

--- express-server/server.js
+++ express-server/server.js
@@ -1,4 +1,10 @@
 /*
+ * server.js | M.Dolce, React Native Portfolio, marti.dolce@29signals.org, 202212
+ * Function ---
+ * This file provides baseline functionality for the app
+ * ------------
+ */
+/*
  * server.js | MDolce, React Native Portfolio, marti.dolce@29signals.org
  * Function ---
  * This file is provides loading capabilities and a splash page while the
@@ -7,47 +13,41 @@
  * ------------
  */
 
-// import modules
-const express = require('express'); //Fast, un-opinionated, minimalist web framework for Node.js
-const bodyParser = require('body-parser');
-const cors = require('cors');  //restricts resources on a web page to be requested from another domain outside the domain from which the first resource was served.
-const dotenv = require('dotenv'); //Loads environment variables from a .env file into process.env
-const morgan = require('morgan'); //HTTP request logger middleware for node.js
-const logger = require('./utils/logger')
 
+// Import Global Modules
+globals = require('./config/globals');
 
-// Create Express app
-const app = express();
+/**
+ * Application Configurations
+ */
+const corsOptions = {
+  origin: 'https://localhost:8081'
+}
+const app = globals.express();
+const PORT = process.env.PORT || 8080;
 
+/**
+ * Body Parser Middleware
+ */
 
-// Middleware
-app.use(morgan('dev'));
-app.use(bodyParser.json());
-app.use(cors({origin: true, credentials: true}));
-app.use(logger);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+ app.use(globals.bodyParser.urlencoded({ extended: true }));
+ app.use(globals.bodyParser.json());
+ app.use(globals.cors(corsOptions));
 
-// Set default routes, directories and paths
-/* app.use(express.static(path.join(__dirname, 'public'))); */
 /**
  * Create a test JSON route
  */
 app.get('/', (req, res) => {
+  console.log(`Initializing the Application`)
   res.json({
-    message: 'Welcome to the User Onboarding Application'
+    message: 'Welcome to the Vacation Rental Application'
   });
 });
+ 
+ /**
+  * Database Configurations
+  */
 
-// Set hostname. Set port use process.env to define port or use 8080
-
-const port = process.env.PORT || 8080;
-
-// Listener: Outputs status of server to console.log
-const server = app.listen(port, () => console.log(`Server listening on port ${port}`));
-
-// Shut down server if we run into problems
-process.on('unhandledRejection', (err) => {
-  console.log(`Error: ${err.message}`)
-  server.close(() => process.exit(1))
-})
+app.listen(PORT, () => {
+   console.log(`Server is listening on PORT ${PORT}`)
+ })
