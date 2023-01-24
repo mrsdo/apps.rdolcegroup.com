3bd3d9f98e6256dff50a561f4cc18d9e6fd117f9
Marti D <marti.dolce@29signals.org>
2023-01-24 10:30:27 -0500

    Adding updates

---
+++ server/README.md
@@ -0,0 +1,3 @@
+# Express Server support
+
+> src https://www.bezkoder.com/node-js-mongodb-auth-jwt/
---
+++ server/config/db.config.js
@@ -0,0 +1,11 @@
+/*
+ * db.config.js | MDolce, React Native Portfolio, marti.dolce@29signals.org
+ * Function ---
+ * This file is provides parameters for setting up MongoDB on localhost.  This can be changed to connect to MongoDB Atlas.
+ * ------------
+ */
+module.exports = {
+  HOST: "localhost",
+  PORT: 27017,
+  DB: "rdolcegroup"
+};
